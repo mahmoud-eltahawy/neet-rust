f12c4ae0dcb694f88954d4d5015060f8cba5adc8
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-10 02:09:15 +0300

    prefix sum tool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
-pub struct Solution;
+use std::ops::{Add, Sub};
 
-impl Solution {
-    pub fn increment(num: i32) -> i32 {
-        num + 1
+pub struct PrefixSum<T: Add<Output = T> + Sub<Output = T> + Clone> {
+    list: Vec<T>,
+}
+
+impl<T: Add<Output = T> + Sub<Output = T> + Clone> PrefixSum<T> {
+    pub fn from(arr: Vec<T>) -> Self {
+        let mut list = Vec::<T>::with_capacity(arr.len());
+        list.push(arr[0].clone());
+        for i in 1..arr.len() {
+            list.push(arr[i].clone() + list[i - 1].clone());
+        }
+
+        Self { list }
+    }
+
+    pub fn from_to(&self, begin: usize, end: usize) -> T {
+        if begin == 0 {
+            return self.list[end].clone();
+        }
+        self.list[end].clone() - self.list[begin - 1].clone()
     }
 }
 
@@ -11,7 +28,10 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn increment() {
-        assert_eq!(Solution::increment(1), 2)
+    fn prefix_sum_test() {
+        let ps = PrefixSum::from(vec![1, 2, 3, 4]);
+        assert_eq!(ps.list, vec![1, 3, 6, 10]);
+
+        assert_eq!(ps.from_to(1, 3), 9);
     }
 }
