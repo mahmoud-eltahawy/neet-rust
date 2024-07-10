51ee187a4308fa33f0c2e43d86f13238fea53804
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-11 02:24:01 +0300

    is anagram

--- src/arrays_and_hashing.rs
+++ src/arrays_and_hashing.rs
@@ -7,6 +7,17 @@ impl Solution {
         let mut exists = HashSet::with_capacity(nums.len());
         !nums.into_iter().all(|x| exists.insert(x))
     }
+
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() || s.is_empty() || t.is_empty() {
+            return false;
+        }
+        let mut s = s.into_bytes();
+        s.sort_unstable();
+        let mut t = t.into_bytes();
+        t.sort_unstable();
+        s == t
+    }
 }
 
 #[cfg(test)]
@@ -21,5 +32,16 @@ mod tests {
             Solution::contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]),
             true
         );
+    }
+    #[test]
+    pub fn is_anagram() {
+        assert_eq!(
+            Solution::is_anagram("anagram".to_string(), "nagaram".to_string()),
+            true
+        );
+        assert_eq!(
+            Solution::is_anagram("rat".to_string(), "car".to_string()),
+            false
+        )
     }
 }
